9db30bc36c379ad528880be3a4e7c5eeb3b7290c
XiNiHa <anesin1109@naver.com>
2021-01-08 01:44:17 +0900

    Change Header Design

--- raspberry-rice-wine/src/components/base/Header.vue
+++ raspberry-rice-wine/src/components/base/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav class="w-full h-10 shadow-md flex justify-between items-center z-50">
+  <nav class="w-full h-10 bg-gray-700 shadow-md flex justify-between items-center z-50 mb-0.5">
     <div class="flex-grow" :class="{ 'pl-16': isMac }" style="-webkit-app-region: drag">
-      <h1 class="font-semibold px-4 align-middle h-auto">{{ $t('app.title') }}</h1>
+      <h1 class="px-5 text-gray-100 align-middle h-auto select-none">{{ $t('app.title') }}</h1>
     </div>
     <div v-if="!isMac">
       <HeaderButton hoverBg="hover:bg-red-500" @click="close">
