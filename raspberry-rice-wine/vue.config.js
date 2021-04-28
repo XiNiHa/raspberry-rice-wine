7082f30e03883e90f2cbea4e3f6283ebdb263b74
XiNiHa <anesin1109@naver.com>
2021-04-28 22:24:15 +0900

    Build Setup

--- raspberry-rice-wine/vue.config.js
+++ raspberry-rice-wine/vue.config.js
@@ -1,7 +1,28 @@
+/* eslint-disable no-template-curly-in-string */
+
 module.exports = {
+  configureWebpack: {
+    devtool: 'source-map'
+  },
   pluginOptions: {
     electronBuilder: {
-      preload: 'src/preload.js'
+      preload: 'src/preload.js',
+      builderOptions: {
+        appId: 'io.github.xiniha.rrw',
+        win: {
+          target: {
+            target: 'nsis',
+            arch: 'x64'
+          }
+        },
+        mac: {
+          target: {
+            target: 'zip',
+            arch: 'universal'
+          },
+          category: 'public.app-category.graphics-design'
+        }
+      }
     }
   }
 }
